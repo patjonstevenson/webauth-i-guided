2ffe3040f814329e9dc6c6d49029aa33b3185c9f
Patrick Stevenson <pat.jon.stevenson@gmail.com>
2019-11-11 09:28:12 -0800

    added configureMiddleware to server.js; created endpoint for POST to /api/hash/ that responds with hashed password

--- api/api-router.js
+++ api/api-router.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
+const bcrypt = require('bcryptjs');
 
 const authRouter = require('../auth/auth-router.js');
 const usersRouter = require('../users/users-router.js');
@@ -8,6 +9,12 @@ router.use('/users', usersRouter);
 
 router.get('/', (req, res) => {
   res.json({ api: "It's alive" });
+});
+
+router.post('/hash', async (req, res) => {
+  const { password } = req.body;
+  const hash = bcrypt.hashSync(password, 14);
+  res.status(201).json({ password: hash });
 });
 
 module.exports = router;
--- api/server.js
+++ api/server.js
@@ -1,8 +1,10 @@
 const express = require('express');
 
 const apiRouter = require('./api-router.js');
+const configureMiddleware = require('./configure-middleware');
 
 const server = express();
+configureMiddleware(server);
 
 server.use('/api', apiRouter);
 
